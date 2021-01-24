4fa4a397c3e1d5bc68c09b41fc404df2e773c070
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-24 09:49:21 +0530

    Game Ready

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+cars = CarManager()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(player.go_up,"Up")
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    cars.create_car()
+    cars.move_cars()
+
+    #Detect collision with car
+    for car in cars.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        cars.level_up()
+        scoreboard.increase_level()
+        scoreboard.update_scoreboard()
+
+
+
+
+screen.exitonclick()
